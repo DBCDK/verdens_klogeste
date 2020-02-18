b7f73169edeb09f98c5280a8532c407819f48a9c
cbo <cbo@dbc.dk>
2020-02-18 13:55:20 +0100

    add code to create + parse queries + examples

---
+++ src/verdens_klogeste/query-env-arg.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json
+import os
+from discovery import Discovery
+import sys
+
+def warn(*args):
+    print(*args, file=sys.stderr)
+    sys.stderr.flush()
+
+def run(query):
+    watson_discovery_apikey = os.environ['WATSON_DISCOVERY_APIKEY']
+    watson_discovery_url = os.environ['WATSON_DISCOVERY_URL']
+
+    DISCOVERY_ENV_NAME = 'vktest_env'
+    DISCOVERY_COLL_NAME = 'vktest_coll'
+    
+    discovery = Discovery(watson_discovery_apikey, watson_discovery_url)
+    env_id = discovery.find_env_id(DISCOVERY_ENV_NAME)
+    coll_id = discovery.find_coll_id(env_id, DISCOVERY_COLL_NAME)
+    response = discovery.discovery.query(env_id, coll_id, query=query)
+    return response
+
+    
+if __name__ == '__main__':
+    query = os.environ['Q']
+    warn("query", query)
+    response = run(query)
+    print(response)
