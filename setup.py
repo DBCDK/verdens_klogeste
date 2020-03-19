a8c7145565d610373958950066ec553d8bdb2470
Jesper Damkjær <jda@dbc.dk>
2020-03-19 10:55:34 +0100

    -

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@
 import glob
 from setuptools import setup, find_packages
 
-setup(name="verdens-klogeste",
+setup(name="verdens_klogeste",
     version="0.1.0",
     package_dir={"": "src"},
     packages=find_packages(where='src'),
     description="",
     scripts=glob.glob('src/bin/*'),
-    provides=["verdens_klogeste"],
     install_requires=["requests", "ibm-watson>=4.2.1", "Wikipedia-API", "sklearn"]
+    include_package_data=True,
+    provides=["verdens_klogeste"],
+    zip_safe=False
 )
